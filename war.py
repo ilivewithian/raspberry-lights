0b7e779f671f3b2928f24846fb4c4f61cd97e987
Rob White <rob.j.white@gmail.com>
2018-02-15 19:03:45 +0000

    Place holder scenes

--- war.py
+++ war.py
@@ -5,13 +5,59 @@ logging.basicConfig()
 from thread import start_new_thread
 status = "pending"
 
+RED_PIN = 17
+GREEN_PIN = 22
+BLUE_PIN = 24
+
 import websocket
+import os
+import sys
+import termios
+import tty
+import pigpio
+import time
 
 try:
 	import thread
 except ImportError:
 	import _thread as thread
 import time
+
+brightChanged = False
+pi = pigpio.pi()
+
+currentR = 0
+currentG = 0
+currentB = 0
+
+def setLights(r,g,b,brightness):
+	global currentR
+	global currentG
+	global currentB
+
+	if r > 255:
+		r = 255
+	if r < 0:
+		r = 0
+	if g > 255:
+		g = 255
+	if g < 0:
+		g = 0
+	if b > 255:
+		b = 255
+	if b < 0:
+		b = 0
+
+	if r != currentR:
+		pi.set_PWM_dutycycle(RED_PIN, r)
+		currentR = r
+	if g != currentG:
+		pi.set_PWM_dutycycle(GREEN_PIN, g)
+		currentG = g
+	if b != currentB:
+		pi.set_PWM_dutycycle(BLUE_PIN, b)
+		currentB = b
+
 
 def on_message(ws, message):
 	global status
@@ -30,7 +76,24 @@ def on_open(ws):
 def war_loop():
 	while True:
 		print status
-		time.sleep(1.5)
+
+		if status == "none":
+			setLights(0, 0, 0, 255)
+			time.sleep(0.5)
+		elif status == "voting":
+			setLights(255, 0, 0, 255)
+			time.sleep(0.5)
+		elif status == "war impending":
+			setLights(0, 255, 255, 255)
+			time.sleep(0.5)
+		elif status == "war active":
+			setLights(0, 0, 255, 255)
+			time.sleep(0.5)
+		elif status == "war stopping":
+			setLights(255, 255, 0, 255)
+			time.sleep(0.5)
+		else:
+			time.sleep(1.5)
 
 start_new_thread(war_loop, ())
 
